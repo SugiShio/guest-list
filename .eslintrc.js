636fc967e85f58c78119faf1affd5cdf375bb104
Shoko Shoko <fruitmachine0@gmail.com>
2020-02-13 00:04:43 +0900

    ESLint設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,11 +14,15 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended'
   ],
-  plugins: [
-    'prettier'
-  ],
-  // add your custom rules here
+  plugins: ['prettier'],
   rules: {
-    'nuxt/no-cjs-in-config': 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-unreachable': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-useless-return':
+      process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'vue/order-in-components':
+      process.env.NODE_ENV === 'production' ? 'error' : 'warn'
   }
 }
