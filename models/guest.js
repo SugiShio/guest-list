5a96f8c0f24f88db9056d3d0a8578a61ee5efef4
Shoko Shoko <fruitmachine0@gmail.com>
2020-02-15 18:32:26 +0900

    ゲストのリスト表示

--- models/guest.js
+++ models/guest.js
@@ -2,13 +2,14 @@ export class Guest {
   constructor(params) {
     this.name = params.name || 'No name guest'
     this.type = params.type
+    this.count = params.count || 0
     this.instruments = this.isPlayer ? params.instruments : []
     this.instrumentMain = this.instruments.includes(params.instrumentMain)
       ? params.instrumentMain
-      : this.instruments[0]
+      : this.instruments[0] || null
     this.instrumentOther = this.instruments.includes('Other')
       ? params.instrumentOther
-      : undefined
+      : null
     this.createdAt = params.createdAt || new Date().getTime()
     if (params.id) this.id = params.id
   }
--- pages/_eventId/list.vue
+++ pages/_eventId/list.vue
@@ -3,19 +3,55 @@ section
   section-head-event
     a(@click='$router.push({name:"eventId-enter"})')
       | Enter page
+  section-content
+    ul.table
+      li.table__item(v-for='block in guestsCategorised')
+        h1.table__title {{ block.instrument }}
+        ul
+          li.guest(
+            v-for='guest in block.guests'
+            :class='{isSub: guest.isSub, isSelected: guest.isSelected}'
+            @click='updateGuestsSelected(guest)')
+            div
+              |{{ guest.name }}
+              span(v-if='block.instrument === "Other"')
+                |({{ guest.instrumentOther }})
+            div(v-if='guest.count') {{ guest.count }}
+  .buttonNewSession(
+    v-if='guestsSelected.length'
+    @click='openModalNewSession')
+    |Create a new session
 </template>
 
 <script>
+import sectionContent from '@/components/sectionContent'
 import sectionHeadEvent from '@/components/sectionHeadEvent'
+import { Guest } from '@/models/guest'
 import { firestore } from '~/plugins/firebase.js'
+const INSTRUMENTS = ['Guitar', 'Keyboard', 'Bass', 'Drums', 'Other']
 export default {
-  components: { sectionHeadEvent },
+  components: { sectionContent, sectionHeadEvent },
   data() {
     return {
-      guests: []
+      guests: [],
+      guestsSelected: []
     }
   },
   computed: {
+    guestsCategorised() {
+      return INSTRUMENTS.map((instrument) => {
+        const guests = this.guests
+          .filter((guest) => guest.instruments.includes(instrument))
+          .map((guest) => {
+            return {
+              ...guest,
+              isSub: guest.instrumentMain !== instrument,
+              isSelected: this.guestsSelected.includes(guest.id)
+            }
+          })
+        return { instrument, guests }
+      })
+    },
     uid() {
       return this.$store.state.uid
     }
@@ -39,25 +75,77 @@ export default {
         .get()
         .then((querySnapShot) => {
           querySnapShot.forEach((doc) => {
-            this.guests.push({ ...doc.data(), id: doc.id })
+            this.guests.push(new Guest({ ...doc.data(), id: doc.id }))
           })
         })
         .catch((error) => {
           throw error
         })
+    },
+    openModalNewSession() {},
+    updateGuestsSelected(guest) {
+      if (this.guestsSelected.includes(guest.id)) {
+        const index = this.guestsSelected.findIndex((g) => g === guest.id)
+        this.guestsSelected.splice(index, 1)
+      } else {
+        this.guestsSelected.push(guest.id)
+      }
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.icon {
-  transition: transform 0.3s;
-  &.isOpen {
-    transform: rotate(180deg);
+.table {
+  display: flex;
+  margin: -5px -5px 50px;
+  &__item {
+    width: 100%;
+    padding: 5px;
+  }
+  &__title {
+    color: #e50012;
+    margin: 10px 0;
   }
 }
-.information {
-  margin: 10px 0;
+.guest {
+  display: flex;
+  justify-content: space-between;
+  border-radius: 3px;
+  cursor: pointer;
+  padding: 5px 10px;
+  transition: background-color 0.3s;
+  &:hover {
+    background-color: #eee;
+  }
+  &.isSub {
+    opacity: 0.6;
+  }
+  &.isSelected {
+    background-color: #e50012;
+    > div {
+      color: #fff;
+      font-weight: bold;
+    }
+  }
+  & + & {
+    margin-top: 5px;
+  }
+}
+.buttonNewSession {
+  position: fixed;
+  right: 20px;
+  bottom: 15px;
+  left: 20px;
+  padding: 10px;
+  background-color: #e50012;
+  border-radius: 3px;
+  text-align: center;
+  color: #fff;
+  transition: opacity 0.3s;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.6;
+  }
 }
 </style>
