68e0ae00a68216895c19512da7d90bdc5ddb5e1e
Shoko Shoko <fruitmachine0@gmail.com>
2020-02-22 14:31:53 +0900

    ちょい修正

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,19 +1,18 @@
 <template lang="pug">
 div
-  |{{ error }}
   error
 </template>
 
 <script>
 import error from '@/components/error'
 export default {
+  components: { error },
   props: {
     error: {
       type: Object,
       required: true
     }
   },
-  components: { error },
   created() {
     this.$store.commit('setError', { error: this.error })
   }
